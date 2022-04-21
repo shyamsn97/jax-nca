7c9a7cdb3c90327e80424a5127c189df1d72a4ab
shyamsn97 <shyamsnair97@gmail.com>
2022-04-21 15:57:17 -0700

    update version

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ with open(path.join(this_directory, 'README.md'), encoding='utf-8') as f:
 
 setup(
     name="jax_nca",
-    version="0.1.0",
+    version="0.1.2",
     url="https://github.com/shyamsn97/jax-nca",
     license='MIT',
 
